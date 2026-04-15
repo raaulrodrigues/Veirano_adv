5fe352547d1624b03f70eb7b9cbcfc26bca0cce3
raaulrodriguees <clovisdavildo@gmail.com>
2026-04-15 10:32:34 -0300

    archive-profissionais

--- archive-profissionais.php
+++ archive-profissionais.php
@@ -1,24 +1,130 @@
 <?php include('header.php'); ?>
-<section id="team-list">
-	<div class="container">
-		<div class="row wrapper-team-cards">
-			<div class="col-lg-6 team-card">
-				<div class="wrapper-img">
-					<a href="#">
-						<img src="img/placeholder-team-jpg" alt="" class="img-fluid">
-					</a>
+	<section id="team-list">
+		<div class="container">
+			<div class="spacer-60"></div>
+			<div class="row wrapper-team-cards">
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-adriana-fernandes.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Adriana Fernandes Rollo</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócia</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>Rio de Janeiro</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 21 3824 1320</p>
+						<a href="#" class="email text-light-blue font-open-sans">adriana.rollo@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
 				</div>
-				<div class="post-info">
-					<a class="title text-dark-blue text-hover-orange" href="#">Nome do Profissional</a>
-					<p class="position text-light-blue text-uppercase">Sócia</p>
-					<p class="location">
-						<i class="bi bi-geo-alt-fill"></i>
-						<span>Rio de Janeiro</span>
-					</p>
-					<a href="#" class="email text-light-blue">nome@veirano.com.br</a>
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-alberto-braganca.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Alberto de Orleans e Bragança</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócio</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>Rio de Janeiro</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 21 3824 4610</p>
+						<a href="#" class="email text-light-blue font-open-sans">alberto.braganca@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-adriana-fernandes.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Alberto Monteiro</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócio</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>São Paulo</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 11 2313 5705</p>
+						<a href="#" class="email text-light-blue font-open-sans">alberto.monteiro@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-alberto-braganca.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Alex Backsmann</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócio</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>São Paulo</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 11 2313 5832</p>
+						<a href="#" class="email text-light-blue font-open-sans">alex.backsmann@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-adriana-fernandes.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Alexandre Verri</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócio</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>São Paulo</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 11 2313 5727</p>
+						<a href="#" class="email text-light-blue font-open-sans">alexandre.verri@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-6 team-card">
+					<div class="wrapper-img">
+						<a href="#">
+							<img src="img/profissional-alberto-braganca.jpg" alt="" class="img-fluid">
+						</a>
+					</div>
+					<div class="post-info">
+						<a class="title text-dark-blue text-hover-orange font-merriweather" href="#">Ali El Hage Filho</a>
+						<p class="position text-light-blue text-uppercase font-open-sans">Sócio</p>
+						<p class="location text-light-gray font-open-sans">
+							<i class="bi bi-geo-alt-fill"></i>
+							<span>Rio de Janeiro</span>
+						</p>
+						<p class="phone text-light-gray font-open-sans">+55 21 3824 4722</p>
+						<a href="#" class="email text-light-blue font-open-sans">ali.hage@veirano.com.br</a>
+						<a href="#" class="plus-link" aria-label="Ver perfil completo">
+							<i class="bi bi-plus"></i>
+						</a>
+					</div>
 				</div>
 			</div>
+			<div class="spacer-60"></div>
 		</div>
-	</div>
-</section>
+	</section>
 <?php include('footer.php'); ?>
