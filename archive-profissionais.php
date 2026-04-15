02c610e9b11e97fc600411e78907b75549043c11
agenciath2 <singularluxurytravel@gmail.com>
2026-04-15 09:20:49 -0300

    profissionais listagem

---
+++ archive-profissionais.php
@@ -0,0 +1,24 @@
+<?php include('header.php'); ?>
+<section id="team-list">
+	<div class="container">
+		<div class="row wrapper-team-cards">
+			<div class="col-lg-6 team-card">
+				<div class="wrapper-img">
+					<a href="#">
+						<img src="img/placeholder-team-jpg" alt="" class="img-fluid">
+					</a>
+				</div>
+				<div class="post-info">
+					<a class="title text-dark-blue text-hover-orange" href="#">Nome do Profissional</a>
+					<p class="position text-light-blue text-uppercase">Sócia</p>
+					<p class="location">
+						<i class="bi bi-geo-alt-fill"></i>
+						<span>Rio de Janeiro</span>
+					</p>
+					<a href="#" class="email text-light-blue">nome@veirano.com.br</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include('footer.php'); ?>
