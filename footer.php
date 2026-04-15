c29bdabd705711ef1cab7d09fff5e4e0cc210d2c
agenciath2 <singularluxurytravel@gmail.com>
2026-04-15 10:12:36 -0300

    home ok

--- footer.php
+++ footer.php
@@ -1,3 +1,94 @@
+    <footer class="bg-gradient-blue font-open-sans">
+        <div class="spacer-50"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 d-flex flex-wrap">
+                    <div class="col-lg-3 wrapper-addresses-cards">
+                        <div class="address-card">
+                            <p class="state text-light-blue text-uppercase">Rio de Janeiro</p>
+                            <p class="address text-white"><b>Av. Bartolomeu Mitre, 770<br/>Leblon<br/>22431-004 - Rio de Janeiro - RJ<br/>BRASIL</b></p>
+                            <span class="divisor bg-orange"></span>
+                            <a href="tel:+55 21 3824-4747" class="tel text-white text-hover-orange">+55 21 3824-4747</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 wrapper-addresses-cards">
+                        <div class="address-card">
+                            <p class="state text-light-blue text-uppercase">São Paulo</p>
+                            <p class="address text-white"><b>Av. Brigadeiro Faria Lima, 3477<br/>16º andar - Itaim Bibi<br/>04538-133 - São Paulo - SP<br/>BRASIL</b></p>
+                            <span class="divisor bg-orange"></span>
+                            <a href="tel:+55 21 3824-4747" class="tel text-white text-hover-orange">+55 11 2313-5700</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 wrapper-addresses-cards">
+                        <div class="address-card">
+                            <p class="state text-light-blue text-uppercase">Porto Alegre</p>
+                            <p class="address text-white"><b>Av. Dr. Nilo Peçanha 2900<br/>15º andar - Chácara das Pedras<br/>91330-001 - Porto Alegre - RS<br/>BRASIL</b></p>
+                            <span class="divisor bg-orange"></span>
+                            <a href="tel:+55 21 3824-4747" class="tel text-white text-hover-orange">+55 51 2121-7500</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 wrapper-addresses-cards">
+                        <div class="address-card">
+                            <p class="state text-light-blue text-uppercase">Brasília</p>
+                            <p class="address text-white"><b>SCS Qd. 9 Lt. C - Parque Cidade Corporate - 12º andar (1203)<br/>70308-200 - Brasília - DF<br/>BRASIL</b></p>
+                            <span class="divisor bg-orange"></span>
+                            <a href="tel:+55 21 3824-4747" class="tel text-white text-hover-orange">+55 61 2106-6600</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 wrapper-addresses-cards">
+                        <div class="address-card">
+                            <p class="state text-light-blue text-uppercase">Belo Horizonte</p>
+                            <p class="address text-white"><b>Av. Getúlio Vargas, 671<br/>7º andar - Savassi<br/>30112-021 Belo Horizonte - MG<br/>BRASIL</b></p>
+                            <span class="divisor bg-orange"></span>
+                            <a href="tel:+55 21 3824-4747" class="tel text-white text-hover-orange">+55 31 3400-0010</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 d-flex flex-column align-items-end">
+                    <div class="w-100 wrapper-logo">
+                        <img src="img/logo-veirano.svg" alt="" class="img-fluid">
+                    </div>
+                    <div class="w-100 wrapper-socials d-flex justify-content-end">
+                        <a class="col-auto text-white text-hover-orange" target="_blank" href="https://www.linkedin.com/company/veirano-advogados">
+                            <i class="bi bi-linkedin"></i>
+                        </a>
+                        <a class="col-auto text-white text-hover-orange" target="_blank" href="https://www.instagram.com/veiranoadvogados/">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                        <a class="col-auto text-white text-hover-orange" target="_blank" href="https://open.spotify.com/user/8y6ra1tbmeavldeloh9i5tfyb?si=b14fcf0fa04c4e71&nd=1&dlsi=889c7071780c47c6">
+                            <i class="bi bi-spotify"></i>
+                        </a>
+                        <a class="col-auto text-white text-hover-orange" target="_blank" href="https://www.youtube.com/@VeiranoBr">
+                            <i class="bi bi-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row copyright">
+                <div class="col-lg-5">
+                    <div class="main-text text-white">
+                        <p>© 2026 Veirano Advogados. Todos os direitos reservados.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7 d-flex justify-content-end footer-links">
+                    <a class="text-white text-hover-orange" href="#">Política de Segurança</a>
+                    <a class="text-white text-hover-orange ms-5" href="#">Política de Privacidade</a>
+                    <a class="text-white text-hover-orange ms-5" href="#">Política de Cookies</a>
+                    <a class="text-white text-hover-orange ms-5" href="#">Termos de Uso</a>
+                </div>
+            </div>
+            <div class="spacer-20"></div>
+            <div class="row developed-by justify-content-end">
+                <div class="col-auto">
+                    <a target="_blank" href="https://www.simbiox.com.br/">
+                        <span>Desenvolvido por</span>
+                        <img src="img/logo-simbiox.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="spacer-50"></div>
+    </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.2/jquery.scrollTo.min.js"></script>
